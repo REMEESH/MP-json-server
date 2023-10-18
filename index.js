0f8622959da5a36b0f091c188602ffb5a2b4bd3b
REMEESH <remeesh97@gmail.com>
2023-10-18 22:10:56 +0530

    mp json

---
+++ index.js
@@ -0,0 +1,26 @@
+//import json-server in index.js file
+const jsonServer = require('json-server')
+
+
+//create a server from media player app
+const mediaPlayerServer =  jsonServer.create()
+
+
+//set up path/route from json file
+const router = jsonServer.router("db.json")
+
+//Returns middlewares used by JSON Server
+const middlewares = jsonServer.defaults()
+
+//set up port for running server
+const port = 4000 || process.env.PORT 
+
+//use milddlewares and router in server
+mediaPlayerServer.use(middlewares)
+mediaPlayerServer.use(router)
+
+//to listen server for resolving request from client
+mediaPlayerServer.listen(port,()=>{
+    console.log(`Media Player server started at port ${port}, and waiting for client request!!!`);
+})
+
